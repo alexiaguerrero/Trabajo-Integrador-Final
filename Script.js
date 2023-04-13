9a5d2483fd3a8a2a0f09cc990302d6192f6e6b0a
alexiag <alexiaguerrero7@gmail.com>
2023-04-13 09:28:59 -0300

    agregue JS, Agregue modificacion en contactame, y formulario

--- Script.js
+++ Script.js
@@ -1,8 +1,31 @@
+const Formulariousuario = document.querySelector("#Formulariousuario");
 
-const Formulariousuario = document.querySelector("#Formulariousuario")
+const contenedorHTML = document.querySelector(".contenedorusuario");
+const usuarios = [];
 
-Formulariousuario.addEventListener("Submit", (event) =>{
-    event.preventDefault()
-    console.log(Formulariousuario.provincia.value)
+const renderizarUsuarios = () => {
+    contenedorHTML.innerHTML = "";
+    for (const usuario of usuarios) {
+        contenedorHTML.innerHTML += `
+            <div class="card-user">
+                <h2>Nombre: ${usuario.nombre}</h2>
+                <p>Email: ${usuario.email}</p>
+                <p>Numero: ${usuario.numero}</p>
+                <p>Provincia: ${usuario.provincia}</p>
+            </div>
+        `;
+    }
+}
 
-})
+Formulariousuario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    usuarios.push({
+        nombre: Formulariousuario.nombre.value,
+        email: Formulariousuario.email.value,
+        numero: Formulariousuario.numero.value,
+        provincia: Formulariousuario.provincia.value
+    });
+    renderizarUsuarios();
+    Formulariousuario.reset();
+    document.querySelector("#mensaje-enviado").textContent = "Su solicitud ha sido enviada. Pronto nos pondremos en contacto con usted.";
+});
