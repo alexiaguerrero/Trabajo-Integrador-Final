b2b469e14540a240a50783f6a7a1f5794b5ff870
alexiag <alexiaguerrero7@gmail.com>
2023-04-14 21:21:46 -0300

    agregue slider en JS

--- Script.js
+++ Script.js
@@ -1,31 +1,10 @@
-const Formulariousuario = document.querySelector("#Formulariousuario");
 
-const contenedorHTML = document.querySelector(".contenedorusuario");
-const usuarios = [];
+var sliderImages = document.querySelectorAll('#slider-main .slider-image');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 4000);
 
-const renderizarUsuarios = () => {
-    contenedorHTML.innerHTML = "";
-    for (const usuario of usuarios) {
-        contenedorHTML.innerHTML += `
-            <div class="card-user">
-                <h2>Nombre: ${usuario.nombre}</h2>
-                <p>Email: ${usuario.email}</p>
-                <p>Numero: ${usuario.numero}</p>
-                <p>Provincia: ${usuario.provincia}</p>
-            </div>
-        `;
-    }
+function nextSlide() {
+  sliderImages[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % sliderImages.length;
+  sliderImages[currentSlide].classList.add('active');
 }
-
-Formulariousuario.addEventListener("submit", (event) => {
-    event.preventDefault();
-    usuarios.push({
-        nombre: Formulariousuario.nombre.value,
-        email: Formulariousuario.email.value,
-        numero: Formulariousuario.numero.value,
-        provincia: Formulariousuario.provincia.value
-    });
-    renderizarUsuarios();
-    Formulariousuario.reset();
-    document.querySelector("#mensaje-enviado").textContent = "Su solicitud ha sido enviada. Pronto nos pondremos en contacto con usted.";
-});
